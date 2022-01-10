c3b7971f7188521616b7f8dc1a8cffdd4b72e5d5
binislam2021 <contactwithmdismail@gmail.com>
2022-01-10 19:42:56 +0600

    Portfolio - PSD to HTML, HomeWork as practice purpose

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var typed = new Typed('.pos-over', {
+        strings: ["michalvina"],
+        typeSpeed: 100,
+        loop: 'true',
+        showCursor: false,
+      });
+});
