534150a272e5f1372b61ea5d87794b870ba4521b
binislam2021 <contactwithmdismail@gmail.com>
2022-01-14 23:54:21 +0600

    Portfolio Nav added

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+  // type JS  plugin added on the Banner Part //
   var typed = new Typed('.pos-over ', {
 
         strings: ["michalvina"],
@@ -8,4 +9,12 @@ $(document).ready(function(){
         typeSpeed: 100,
         backSpeed: 100,
       });
+  // =============type JS Ends============= //
+
+  // Active class jQuery added on the nav bar in portfolio //
+      $('#portfolio li').on('click', function(){
+        $(this).addClass('active').siblings().removeClass('active');
+      });
+  // =============Adding class Ends============= //
+
 });
