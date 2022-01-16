dbd3989aec2eb768a1e3de120d8e0145220b4e03
binislam2021 <contactwithmdismail@gmail.com>
2022-01-16 23:37:12 +0600

    Pricing Part added

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
 
   // type JS  plugin added on the Banner Part //
-  var typed = new Typed('.pos-over ', {
+  var typed = new Typed('.pos-over', {
 
         strings: ["michalvina"],
         loop: 'true',
         startDelay: 1000,
-        typeSpeed: 100,
-        backSpeed: 100,
+        typeSpeed: 150,
+        backSpeed: 50,
+        backDelay: 1000,
       });
   // =============type JS Ends============= //
 
